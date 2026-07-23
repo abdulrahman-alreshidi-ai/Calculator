2717b03ab7be0c3ee5417d4049ea91f671f866b9
abdulrahman-alreshidi-ai <abdalrhmansaadd@gmail.com>
2026-07-23 03:50:44 +0300

    Add menu function for Python Calculator

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 
+def menu():
+    print("=" * 35)
+    print("        Python Calculator")
+    print("=" * 35)
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Power")
+    print("6. Modulus")
+    print("7. Exit")
+
+
+menu()
